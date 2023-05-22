c0cdc51becc10e4197519db530f9d36f345a135a
ashikur-rahman-10 <ashikur.rahman3912@gmail.com>
2023-05-23 03:17:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a46jnic.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        const booksCollections = client.db("islamHouseDB").collection("books");
+        // Insert Book in db
+        app.post('/books', async (req, res) => {
+            const book = req.body;
+            const result = await booksCollections.insertOne(book)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Islam House is runnig......')
+})
+
+app.listen(port, () => {
+    console.log(`Islam House listening on port ${port}`)
+})
